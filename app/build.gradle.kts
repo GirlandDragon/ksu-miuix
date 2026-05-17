fd534346682ebee7e353c6ccbd7b74853c57a694
GirlandDragon <3819987155@qq.com>
2026-05-17 12:21:44 +0000

    fix: override aar metadata to relax SDK requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,10 +45,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-}
-
-afterEvaluate {
-    tasks.matching { it.name.contains("checkAarMetadata") }.configureEach { enabled = false }
+    aarMetadata {
+        minCompileSdk = 26
+        minAgpVersion = "8.7.3"
+    }
 }
 
 dependencies {
